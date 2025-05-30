efd74d4aea7aae8f1766d5f710b9b7fd879b7152
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-30 22:13:20 +0530

    Create Program2.java

---
+++ Module-01/Codes/30th May/Program2.java
@@ -0,0 +1,21 @@
+public class Main
+{ 
+	public static void main(String[] args) {
+		int a = 50;
+		System.out.println("Value of a is:" +  a);
+		
+		double temp = 98.6;
+		System.out.println("Value of temp is: " + temp);
+		
+		String abc = "Bootcamp";
+		System.out.println("Value of abc is:" + abc);
+		
+		char ch = 'c';
+		System.out.println("Value of ch is: " + ch);
+		
+		boolean d = false;
+		System.out.println("Value of d is: " + d);
+
+
+	}
+}
