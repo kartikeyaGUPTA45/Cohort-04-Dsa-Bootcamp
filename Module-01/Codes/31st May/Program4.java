be5cedbe3ee6a8b8d89f130fe63d1066811eddd3
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-31 22:49:39 +0530

    Create Program4.java

---
+++ Module-01/Codes/31st May/Program4.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+        
+	public static void main(String[] args) {
+		int p =10000, r = 8, t = 2;
+		
+		double si = ((double)p*r*t)/100;
+		
+		System.out.println(si);
+	}
+}
