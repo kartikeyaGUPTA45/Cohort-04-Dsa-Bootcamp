65b94e961dea47a5fdf2abe1a46261365e5341bf
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-31 22:34:15 +0530

    Create Program3.java

---
+++ Module-01/Codes/31st May/Program3.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+        
+	public static void main(String[] args) {
+		double c = -40;
+		double f = (c*9/5)+32;
+		
+		System.out.println(f);
+		
+	}
+}
