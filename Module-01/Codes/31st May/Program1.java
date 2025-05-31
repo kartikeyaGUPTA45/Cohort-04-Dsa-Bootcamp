9052566f2f8f00a986f65ddf7cbaad0511fd2dbc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-31 21:24:35 +0530

    Update Program1.java

--- Module-01/Codes/31st May/Program1.java
+++ Module-01/Codes/31st May/Program1.java
@@ -1,1 +1,29 @@
+/******************************************************************************
 
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+        
+	public static void main(String[] args) {
+		int a = 24;
+		int b = 7;
+		
+		int c = a+b;
+		int d = a-b;
+		int e = a*b;
+		double f = (double)a/b;
+		int g = a%b;
+		
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(e);
+		System.out.println(f);
+		System.out.println(g);
+		
+	}
+}
