3d35a7dd04da8faef1b55cb3d5b3f43c5a461818
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-31 22:21:19 +0530

    Update Program2.java

--- Module-01/Codes/31st May/Program2.java
+++ Module-01/Codes/31st May/Program2.java
@@ -23,6 +23,9 @@ public class Main
         
         System.out.println(a >= b); // false -> when a is neither greater than a nor equal to a
         System.out.println(a >= c); // true -> a is equal to c
+        
+        // Ternary operator
+        System.out.println(a >= b ? 35 : 65) // 35 is the o/p when condition is true and 65 is the o/p when condition is false
 		
 	}
 }
