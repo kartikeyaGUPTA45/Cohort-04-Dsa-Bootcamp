6565619207a210dcdb899c08a783bce8dad9b906
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-05-31 22:17:05 +0530

    Create Program2.java

---
+++ Module-01/Codes/31st May/Program2.java
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+        
+	public static void main(String[] args) {
+		int a = 10;
+        int b = 20;
+        int c = 10;
+        
+        // System.out.println(a == b);
+        // System.out.println(a != b);
+        // System.out.println(a > b);
+        // System.out.println(a < b);
+        // System.out.println(a <= b); // true -> when b is greater than a
+        // System.out.println(a <= c); // true -> a is equal to c
+        
+        System.out.println(a >= b); // false -> when a is neither greater than a nor equal to a
+        System.out.println(a >= c); // true -> a is equal to c
+		
+	}
+}
