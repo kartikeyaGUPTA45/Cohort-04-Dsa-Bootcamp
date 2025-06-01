eab842536361ed5e7c33215dd9ffe9324dd01a31
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-01 11:26:02 +0530

    Create Program2.java

---
+++ Module-01/Codes/1st June/Program2.java
@@ -0,0 +1,17 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		double c = scn.nextDouble();
+		double f = (c*9/5)+32;
+		System.out.println(f);
+	}
+}
