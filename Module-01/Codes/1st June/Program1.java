d1020d526aac4862eb1c1ec570dc37554639525d
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-01 10:33:37 +0530

    Update Program1.java

--- Module-01/Codes/1st June/Program1.java
+++ Module-01/Codes/1st June/Program1.java
@@ -10,13 +10,19 @@ public class Main
 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		System.out.println("Enter first number:");
-		int a = scn.nextInt();
-		System.out.println("Enter second number:");
-		int b = scn.nextInt();
+// 		System.out.println("Enter first number:");
+// 		int a = scn.nextInt();
+// 		System.out.println("Enter second number:");
+// 		int b = scn.nextInt();
 		
-		int c = a+b;
+// 		int c = a+b;
 		
-	    System.out.println("Sum of a+b is:" + c);
+// 	    System.out.println("Sum of a+b is:" + c);
+	    
+	   // String str = scn.next();
+	    String str2 = scn.nextLine();
+	    System.out.println("String is: " + str2);
+	    
+	    
 	}
 }
