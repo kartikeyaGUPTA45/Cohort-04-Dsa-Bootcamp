dafc41782cae2a83aaeff4a99717c4539b64edf1
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-06 21:27:30 +0530

    Create Program2.java

---
+++ Module-01/Codes/06th June/Program2.java
@@ -0,0 +1,23 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter your age: ");
+		int age = scn.nextInt();
+		
+		if (age >= 18) {
+		    System.out.println("Eligible for Voting");
+		} else {
+		    System.out.println("Not Eligible for Voting");
+		}
+	}
+}
