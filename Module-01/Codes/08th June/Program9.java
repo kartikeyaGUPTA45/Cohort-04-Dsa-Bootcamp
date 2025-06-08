dbc2d9cc34b88cf46ec9e4d836a39173a731ee28
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-08 11:52:11 +0530

    Update Program9.java

--- Module-01/Codes/08th June/Program9.java
+++ Module-01/Codes/08th June/Program9.java
@@ -1,1 +1,25 @@
 
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		Scanner scn = new Scanner(System.in);
+		System.out.println("Enter the number: ");
+		int n = scn.nextInt();
+		int sum = 0;
+// 		for(int i=n;i>0;i/=10) { 
+// 		  sum += (i%10);
+// 		}
+		
+
+        
+        int i=n;
+        while(i>0) {
+            sum += (i%10);
+            i/=10;
+        }
+        
+        System.out.println(sum);
+	}
+}
