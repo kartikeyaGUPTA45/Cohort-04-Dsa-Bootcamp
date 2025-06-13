d4ba24cac523c8a0723f04c1919c3683ba437f84
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-13 22:33:05 +0530

    Create Program7.java

---
+++ Module-01/Codes/13th June/Program7.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 5;
+		
+		int i = 1;
+		
+		while(i<=5) {
+		    int j = 1;
+		    while(j <= 5) {
+		        System.out.print("*");
+		        j++;
+		    }
+		    
+		    System.out.println();
+		    i++;
+		}
+	}
+}
