e999dce0beb92d93383be2a1262965c4be1aa592
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-14 22:30:17 +0530

    Create Program8.java

---
+++ Module-01/Codes/14th June/Program8.java
@@ -0,0 +1,28 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int n = 5;
+		int nst = 1;
+		int nsp = n/2;
+		for(int i=1;i<=n;i++) {
+		   for(int j=1;j<=nsp;j++) {
+		       System.out.print(" ");
+		   } 
+		   
+		   for(int j=1;j<=nst;j++) {
+		       System.out.print("*");
+		   }
+		   
+		   if (i <= n/2) {
+		       nst+=2;
+		       nsp-=1;
+		   } else {
+		       nst-=2;
+		       nsp+=1;
+		   }
+		   
+		   System.out.println();
+		}
+		
+	}
+}
