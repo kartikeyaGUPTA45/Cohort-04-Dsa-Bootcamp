b600d29387bebb45df9c41d041068efe2cd42340
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-15 11:47:40 +0530

    Create Program1.java

---
+++ Module-01/Codes/15th June/functions/Program1.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    
+    public static void printMessage() {
+        System.out.println("DSA Bootcamp 4");
+    }
+    
+    
+	public static void main(String[] args) {
+        printMessage();
+	}
+}
