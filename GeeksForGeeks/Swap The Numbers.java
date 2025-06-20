d5711dffd16748247dab75ff893189e5fa9bd889
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-20 23:05:29 +0530

    Update and rename Swap The Numbers to Swap The Numbers.java

---
+++ GeeksForGeeks/Swap The Numbers.java
@@ -0,0 +1,19 @@
+// Link: https://www.geeksforgeeks.org/problems/swap-the-numbers/1?page=1&category=Java,Functions&sortBy=submissions
+
+Code: 
+
+// User function Template for Java
+
+class Solution {
+    public void swap(int a, int b) {
+        // code here
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println(a + " " + b);
+    }
+}
+
+
+
