bd9721b1d53efa235edf8a806dd77703ffbc4385
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-20 22:54:07 +0530

    Update and rename Even Odd Game to Even Odd Game.java

---
+++ GeeksForGeeks/Even Odd Game.java
@@ -0,0 +1,13 @@
+// Link: https://www.geeksforgeeks.org/problems/even-odd-game/1?page=2&category=Java,Functions&sortBy=submissions
+
+Code:
+// User function Template for Java
+class Solution {
+    public static String utility(int n) {
+
+        // write your code here
+        if (n%2 == 0) return "Friend";
+        return "You";
+    }
+}
+
