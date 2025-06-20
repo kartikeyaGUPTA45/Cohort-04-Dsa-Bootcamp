ec116326bd9e78a8c150d2c667cb97a9b1ac6dc0
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-20 22:32:09 +0530

    Update and rename AP Term to AP Term.java

---
+++ GeeksForGeeks/AP Term.java
@@ -0,0 +1,14 @@
+// Link: https://www.geeksforgeeks.org/problems/ap-term/1?page=3&category=Java,Functions&sortBy=submissions
+
+Code
+
+class Solution {
+    public int apTerm(int a, int d, int n) {
+        // code here
+        
+        int ans = a + (n-1)*d;
+        return ans;
+    }
+}
+
+
