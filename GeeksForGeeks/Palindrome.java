e5ce0d57453ee5883fa659ed3590feb687029da2
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-20 23:00:57 +0530

    Update Palindrome.java

--- GeeksForGeeks/Palindrome.java
+++ GeeksForGeeks/Palindrome.java
@@ -1,1 +1,19 @@
 // Link: https://www.geeksforgeeks.org/problems/palindrome0746/1
+
+Code: 
+// User function Template for Java
+
+class Solution {
+    public boolean isPalindrome(int n) {
+        // Code here
+        int temp = n, ans = 0;
+        
+        while(temp > 0) {
+            int rem = temp%10;
+            ans = ans*10 + rem;
+            temp /= 10;
+        }
+        
+        return (n == ans);
+    }
+}
