ecb6d3ac66c114ab5ea9b299a4051c06d05d525f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-20 22:51:05 +0530

    Update and rename Decision Making in Java to Decision Making in Java.java

---
+++ GeeksForGeeks/Decision Making in Java.java
@@ -0,0 +1,12 @@
+// Link: https://www.geeksforgeeks.org/problems/java-if-else-decision-making0924/1?page=1&category=Java,Functions&sortBy=submissions
+
+Code:
+// User function Template for Java
+class Solution {
+    public static String compareNM(int n, int m) {
+        // code here
+        if (n < m) return "lesser";
+        else if (n == m) return "equal";
+        return "greater";
+    }
+}
