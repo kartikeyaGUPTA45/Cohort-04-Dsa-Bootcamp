c5e54ccffb263bf2d84dc7c6cd2fce3eee96957c
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-21 23:21:46 +0530

    Update Binary to decimal.java

--- GeeksForGeeks/Number system/Binary to decimal.java
+++ GeeksForGeeks/Number system/Binary to decimal.java
@@ -1,1 +1,20 @@
 Link: https://www.geeksforgeeks.org/problems/binary-number-to-decimal-number3525/1
+
+Code: 
+
+// User function Template for Java
+
+class Solution {
+    public int binaryToDecimal(String b) {
+        // Code here
+        int ans = 0, pow = 1;
+        
+        for(int i=b.length()-1;i>=0;i--) {
+            int d = b.charAt(i) - '0';
+            ans += (d*pow);
+            pow*=2;
+        }
+        
+        return ans;
+    }
+}
