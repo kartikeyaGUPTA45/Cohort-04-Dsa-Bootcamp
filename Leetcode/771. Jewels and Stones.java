9f484e9fe87bd2ffcd8261f729c841996a9835dc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 11:11:33 +0530

    Update 771. Jewels and Stones.java

--- Leetcode/771. Jewels and Stones.java
+++ Leetcode/771. Jewels and Stones.java
@@ -1,1 +1,17 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for(int i=0;i<J.length();i++) {
+            char ch1 = J.charAt(i);
+            for(int j=0;j<S.length();j++) {
+                char ch2 = S.charAt(j);
 
+                if (ch1 == ch2) {
+                    count+=1;
+                }
+            }
+        }
+
+        return count;
+    }
+}
