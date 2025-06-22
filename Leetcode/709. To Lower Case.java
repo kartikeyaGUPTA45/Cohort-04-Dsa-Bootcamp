3490907abef12b785d15aab78f2c10da5f021490
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 11:07:23 +0530

    Update 709. To Lower Case.java

--- Leetcode/709. To Lower Case.java
+++ Leetcode/709. To Lower Case.java
@@ -1,1 +1,17 @@
+class Solution {
+    public String toLowerCase(String s) {
+        String ans = "";
 
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                ch = (char)(ch-'A'+'a');
+            }
+
+            ans += ch;
+        }
+
+        return ans;
+    }
+}
