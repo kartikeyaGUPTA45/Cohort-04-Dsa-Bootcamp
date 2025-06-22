4671f0b6249cc067bdbf9b9d9f2b531324729dbf
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 11:42:49 +0530

    Update 1945. Sum of Digits of String After Convert.java

--- Leetcode/1945. Sum of Digits of String After Convert.java
+++ Leetcode/1945. Sum of Digits of String After Convert.java
@@ -1,1 +1,25 @@
 
+class Solution {
+    public int getLucky(String s, int k) {
+        String num = "";
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+            num += ((ch-'a'+1)+"");
+        } 
+
+        // zbax -> 262124  => 2 + 6 + 2 + 1 + 2 + 4 => 17
+        int ans = 0;
+        for(int i=1;i<=k;i++) {
+            ans = 0;
+            for(int j=0;j<num.length();j++) {
+                char ch = num.charAt(j);
+                int d = ch-'0';
+                ans += d;
+            }
+            num = ans + "";
+        }
+
+        return ans;
+    }
+}
