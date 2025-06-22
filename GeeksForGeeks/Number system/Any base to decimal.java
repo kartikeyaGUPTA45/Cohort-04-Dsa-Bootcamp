09575c04bcd0fe6101380375b133a300de04fa69
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 10:16:36 +0530

    Update Any base to decimal.java

--- GeeksForGeeks/Number system/Any base to decimal.java
+++ GeeksForGeeks/Number system/Any base to decimal.java
@@ -1,1 +1,28 @@
 Link: https://www.geeksforgeeks.org/problems/convert-from-any-base-to-decimal3736/1
+
+Code: 
+// User function Template for Java
+
+class Solution {
+    public static int getDigit(char ch) {
+        if (ch >= 'A') return ch-'A'+10;
+        return ch-'0';
+    }
+    
+    static int decimalEquivalent(String n, int b) {
+        // code here
+        int ans = 0;
+        int pow = 1;
+        
+        for(int i = n.length()-1;i>=0;i--) {
+            char ch = n.charAt(i);
+            int d = getDigit(ch);
+            if (d >= b) return -1;
+            
+            ans += (d*pow);
+            pow*=b;
+        }
+        
+        return ans;
+    }
+}
