55b0347eff865f12ff2a90f2342df8f8b1fed9f9
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 11:02:57 +0530

    Update 125. Valid Palindrome.java

--- Leetcode/125. Valid Palindrome.java
+++ Leetcode/125. Valid Palindrome.java
@@ -1,1 +1,33 @@
 
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = "";
+
+        for(int i=0;i<s.length();i++) {
+            char ch = s.charAt(i);
+
+            if (ch >= 'A' && ch <= 'Z') {
+                char updatedCh = (char)(ch-'A'+'a');
+                str+=updatedCh;
+            } else if (ch >= 'a' && ch <= 'z') {
+                str += ch;
+            } else if (ch >= '0' && ch <= '9') {
+                str += ch;
+            }
+        }
+
+        int i = 0, j = str.length()-1;
+
+        while(i<j) {
+            char chI = str.charAt(i);
+            char chJ = str.charAt(j);
+
+            if (chI != chJ) return false;
+
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+}
