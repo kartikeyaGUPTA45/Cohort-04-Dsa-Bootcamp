e3aaeb7a76d799c4cdf48ddeceaa25f69d14fee0
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-22 11:56:45 +0530

    Update 1957. Delete Characters to Make Fancy String.java

--- Leetcode/1957. Delete Characters to Make Fancy String.java
+++ Leetcode/1957. Delete Characters to Make Fancy String.java
@@ -1,1 +1,22 @@
+class Solution {
+    public String makeFancyString(String s) {
+        StringBuilder ans = new StringBuilder();
+        ans.append(s.charAt(0));
 
+        if (s.length() == 1) {
+            return ans.toString();
+        }
+
+        for(int i=1;i<s.length()-1;i++) {
+            char ch = s.charAt(i);
+            char chB = s.charAt(i-1);
+            char chA = s.charAt(i+1);
+
+            if ((ch == chA) && (ch == chB)) {} 
+            else ans.append(ch);
+        }
+
+        ans.append(s.charAt(s.length()-1));
+        return ans.toString();
+    }
+}
