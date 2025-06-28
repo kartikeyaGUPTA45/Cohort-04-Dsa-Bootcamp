b9c4def75d06c797114eeab2c51d4aed489f9302
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-28 21:33:53 +0530

    Create First Index And Last Index.java

---
+++ Hackkerrank/Classwork Questions/First Index And Last Index.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static int lastIndex(int arr[], int d) {
+//         int last = -1;
+        
+//         for(int i=0;i<arr.length;i++) {
+//             if (arr[i] == d) {
+//                 last = i;
+//             }
+//         }
+        
+//         return last;
+        
+        for(int i=arr.length-1;i>=0;i--) {
+            if (arr[i] == d) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static int firstIndex(int arr[], int d) {
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i] == d) {
+                return i;
+            }
+        }
+        
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n;i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        int d = scn.nextInt();
+        
+        System.out.println(firstIndex(arr, d));
+        System.out.println(lastIndex(arr,d));
+    }
+}
