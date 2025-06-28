7f3bd1eb3e5ba754838fbebeebdb501b6faa6a10
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-06-28 21:40:38 +0530

    Update First Index And Last Index.java

--- Hackkerrank/Classwork Questions/First Index And Last Index.java
+++ Hackkerrank/Classwork Questions/First Index And Last Index.java
@@ -32,6 +32,41 @@ public class Solution {
         
         return -1;
     }
+    
+    public static int [] findIndex(int arr[], int d) {
+        int ans [] = new int [2];
+        
+//         for(int i=0;i<arr.length;i++) {
+//             if (arr[i] == d) {
+//                 ans[0] = i;
+//                 break;
+//             }
+//         }
+        
+        
+//         for(int i=arr.length-1;i>=0;i--) {
+//             if (arr[i] == d) {
+//                 ans[1] = i;
+//                 break;
+//             }
+//         }
+        
+        ans[0] = -1;
+        
+        for(int i=0;i<arr.length;i++) {
+            if (arr[i] == d && ans[0] == -1) {
+                ans[0] = i;
+            }
+            
+            if (arr[i] == d) {
+                ans[1] = i;
+            }
+        } 
+        
+        
+        
+        return ans;
+    }
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
@@ -45,7 +80,12 @@ public class Solution {
         
         int d = scn.nextInt();
         
-        System.out.println(firstIndex(arr, d));
-        System.out.println(lastIndex(arr,d));
+        // System.out.println(firstIndex(arr, d));
+        // System.out.println(lastIndex(arr,d));
+        
+        int ans[] = findIndex(arr,d);
+        
+        System.out.println(ans[0]);
+        System.out.println(ans[1]);
     }
 }
