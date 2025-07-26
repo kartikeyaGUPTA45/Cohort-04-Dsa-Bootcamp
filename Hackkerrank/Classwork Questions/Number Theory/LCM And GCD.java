fea8bb66041fd260c1c6797dc8d0bc5081058be5
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-07-26 22:49:25 +0530

    Create LCM And GCD.java

---
+++ Hackkerrank/Classwork Questions/Number Theory/LCM And GCD.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static int gcd(int a, int b) {
+        if (b == 0) return a;
+        return gcd(b,a%b);
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int a = scn.nextInt();
+        int b = scn.nextInt();
+        
+        if (a < 0) a = a*(-1);
+        if (b < 0) b = b*(-1);
+        
+        int g = gcd(a,b);
+        int lcm = (a*b)/(g);
+        
+        System.out.println(g);
+        System.out.println(lcm);
+    }
+}
