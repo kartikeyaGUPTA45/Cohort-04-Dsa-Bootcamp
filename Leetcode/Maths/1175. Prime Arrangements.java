a354a389c8e4f4684e0e7225c4072820e4179ba9
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-02 21:34:34 +0530

    Create 1175. Prime Arrangements.java

---
+++ Leetcode/Maths/1175. Prime Arrangements.java
@@ -0,0 +1,46 @@
+class Solution {
+    private int primeSieve(int n) {
+        int arr[] = new int[n+1];
+        Arrays.fill(arr,true);
+
+        arr[0] = false;
+        arr[1] = false;
+
+        for(int i=2;i*i<=n;i++) {
+            if (arr[i] == true) {
+                for(int j = i*i;j<=n;j+=i) {
+                    arr[j] = false;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i=2;i<=n;i++) {
+            if (arr[i] == true) {
+                cnt+=1;
+            }
+        }
+
+        return cnt;
+    }
+
+    private int factorial(int n, int mod) {
+        int ans = 1;
+        for(int i=2;i<=n;i++) {
+            ans = (ans*i)%mod;
+        }
+
+        return ans;
+    }
+
+    public int numPrimeArrangements(int n) {
+        int cntPrime = primeSieve(n);
+        int cntNonPrime = n - cntPrime;
+        int mod = 1000000007;
+
+        int x = factorial(cntPrime, mod);
+        int y = factorial(cntNonPrime, mod);
+
+        return (x*y)%mod;
+    }
+}
