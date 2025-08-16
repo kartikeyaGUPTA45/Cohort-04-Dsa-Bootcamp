5e816a3a7ce79b28b791fb2faefae4046eb88131
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 23:01:32 +0530

    Update 338. Counting Bits.java

--- Module-02/Bits Manipulation/338. Counting Bits.java
+++ Module-02/Bits Manipulation/338. Counting Bits.java
@@ -1,1 +1,20 @@
 
+class Solution {
+    public int countSetBits(int n) {
+        int ans = 0; 
+        while(n != 0) {
+            ans += 1;
+            n = (n&(n-1));
+        }
+        return ans;
+    }
+
+    public int[] countBits(int n) {
+        int ans[] = new int[n+1];
+        for(int i=1;i<=n;i++) {
+            ans[i] = countSetBits(i);
+        }
+
+        return ans;
+    }
+}
