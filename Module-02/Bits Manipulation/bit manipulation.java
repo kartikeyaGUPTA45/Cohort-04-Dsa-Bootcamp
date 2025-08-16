4064ff05bb5d54a4a38ff8f622443acb5c0f0fba
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 22:40:38 +0530

    Update bit manipulation.java

--- Module-02/Bits Manipulation/bit manipulation.java
+++ Module-02/Bits Manipulation/bit manipulation.java
@@ -1,1 +1,17 @@
 // Problem link: https://www.geeksforgeeks.org/problems/bit-manipulation-1666686020/1#
+
+
+// User function Template for Java
+
+class Solution {
+    static void bitManipulation(int N, int i) {
+        // code here
+        i = i-1;
+        int getBit = ((N>>i)&1);
+        int setBit = ((1<<i)|N);
+        int clearBit = (~(1<<i)&N);
+        
+        System.out.print(getBit + " " + setBit + " " + clearBit);
+    }
+}
+
