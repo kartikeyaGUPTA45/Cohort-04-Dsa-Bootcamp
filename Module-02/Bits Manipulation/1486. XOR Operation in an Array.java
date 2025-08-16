24aae918f44b4c9406c3bc6e214c3a8d3628b339
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 22:46:23 +0530

    Update 1486. XOR Operation in an Array.java

--- Module-02/Bits Manipulation/1486. XOR Operation in an Array.java
+++ Module-02/Bits Manipulation/1486. XOR Operation in an Array.java
@@ -1,1 +1,12 @@
 
+class Solution {
+    public int xorOperation(int n, int start) {
+        int ans = start;
+        for(int i=2;i<=n;i++) {
+            start +=2;
+            ans ^= start;
+        }
+
+        return ans;
+    }
+}
