7cae759f952487594538a241a1bda5161b468ed3
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 22:38:46 +0530

    Update Check K-th Bit.java

--- Module-02/Bits Manipulation/Check K-th Bit.java
+++ Module-02/Bits Manipulation/Check K-th Bit.java
@@ -1,1 +1,8 @@
 //Problem link: https://www.geeksforgeeks.org/problems/check-whether-k-th-bit-is-set-or-not-1587115620/1
+
+class CheckBit {
+    static boolean checkKthBit(int n, int k) {
+        // code here
+        return (((n>>k)&1) == 1) ? true : false;
+    }
+}
