b6246321426594af08ac03bcce9c4b64a42ae6fc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 22:43:34 +0530

    Update 136. Single Number.java

--- Module-02/Bits Manipulation/136. Single Number.java
+++ Module-02/Bits Manipulation/136. Single Number.java
@@ -1,1 +1,11 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
 
+        for(int ele : nums) {
+            ans ^= ele;
+        }
+
+        return ans;
+    }
+}
