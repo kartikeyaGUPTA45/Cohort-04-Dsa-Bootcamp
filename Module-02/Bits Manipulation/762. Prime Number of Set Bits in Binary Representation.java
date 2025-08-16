fcb1f54b86b584c2b53268d35a227f1a1fb7bf85
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-16 22:52:31 +0530

    Update 762. Prime Number of Set Bits in Binary Representation.java

--- Module-02/Bits Manipulation/762. Prime Number of Set Bits in Binary Representation.java
+++ Module-02/Bits Manipulation/762. Prime Number of Set Bits in Binary Representation.java
@@ -1,1 +1,37 @@
 
+class Solution {
+
+    public boolean isPrime(int n) {
+        if (n == 1) {
+            return false;
+        }
+
+        for(int i=2;i*i<=n;i++) {
+            if (n%i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public int countSetBits(int n) {
+        int ans = 0; 
+        for(int i=0;i<32;i++) {
+            ans += ((n>>i)&1);
+        }
+        return ans;
+    }
+    
+    public int countPrimeSetBits(int left, int right) {
+        int ans = 0;
+        for(int i=left;i<=right;i++) {
+            int cntSB = countSetBits(i);
+            if (isPrime(cntSB)) {
+                ans+=1;
+            }
+        }
+
+        return ans;
+    }
+}
