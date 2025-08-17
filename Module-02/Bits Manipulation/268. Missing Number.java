48203ef07361b4f61452fb400c135fbfe4175afc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 11:51:43 +0530

    Update 268. Missing Number.java

--- Module-02/Bits Manipulation/268. Missing Number.java
+++ Module-02/Bits Manipulation/268. Missing Number.java
@@ -1,1 +1,17 @@
 
+class Solution {
+    public int missingNumber(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+
+        for(int i=0;i<=n;i++) {
+            ans ^= i;
+        }
+
+        for(int i=0;i<n;i++) {
+            ans ^= (nums[i]);
+        }
+
+        return ans;
+    }
+}
