a280795bdee9729dfee3df42a517691d84771aae
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 10:47:37 +0530

    Update 477. Total Hamming Distance.java

--- Module-02/Bits Manipulation/477. Total Hamming Distance.java
+++ Module-02/Bits Manipulation/477. Total Hamming Distance.java
@@ -1,1 +1,20 @@
 
+class Solution {
+
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0;
+        int n = nums.length;
+        for(int i=0;i<32;i++) {
+            int cSB = 0;
+            for(int j=0;j<n;j++) {
+                int N = nums[j];
+                cSB += ((N>>i)&1);
+            }
+
+            int cUSB = n - cSB;
+            ans += (cSB*cUSB);
+        }
+
+        return ans;
+    }
+}
