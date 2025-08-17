eb05e3d878e135f755661c6e48f4e9d607dec05f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 11:04:47 +0530

    Update Odd or Even.java

--- Module-02/Bits Manipulation/Odd or Even.java
+++ Module-02/Bits Manipulation/Odd or Even.java
@@ -1,1 +1,8 @@
 // Problem Link: https://www.geeksforgeeks.org/problems/odd-or-even3618/1
+
+class Solution {
+    static boolean isEven(int n) {
+        // code here
+        return ((n&1) == 0);
+    }
+}
