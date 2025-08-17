2036870575f248ce1df0ed7d4847ea99a3f4a8f6
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 11:54:52 +0530

    Update 2220. Minimum Bit Flips to Convert Number.java

--- Module-02/Bits Manipulation/2220. Minimum Bit Flips to Convert Number.java
+++ Module-02/Bits Manipulation/2220. Minimum Bit Flips to Convert Number.java
@@ -1,1 +1,17 @@
+class Solution {
 
+    public int countSetBits(int n) {
+        int ans = 0;
+        while(n != 0) {
+            ans +=1;
+            n = (n&(n-1));
+        }
+
+        return ans;
+    }
+
+    public int minBitFlips(int start, int goal) {
+        int num = start ^ goal;
+        return countSetBits(num);
+    }
+}
