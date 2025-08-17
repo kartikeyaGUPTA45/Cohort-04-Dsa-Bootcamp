97e1e77adef8df8d3ebe1faeea3987377ee582b5
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 10:33:14 +0530

    Update 461. Hamming Distance.java

--- Module-02/Bits Manipulation/461. Hamming Distance.java
+++ Module-02/Bits Manipulation/461. Hamming Distance.java
@@ -1,1 +1,16 @@
+class Solution {
+    public int countSetBits(int num) {
+        int ans = 0;
+        while(num != 0) {
+            ans += 1;
+            num = (num & (num-1));
+        }
 
+        return ans;
+    }
+
+    public int hammingDistance(int x, int y) {
+        int num = x^y;
+        return countSetBits(num);
+    }
+}
