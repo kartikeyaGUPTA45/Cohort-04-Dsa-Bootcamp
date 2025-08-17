7791d1a57a60b5506d2d1988271020b1a9f68fdc
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-17 11:44:55 +0530

    Update Set the rightmost unset bit.java

--- Module-02/Bits Manipulation/Set the rightmost unset bit.java
+++ Module-02/Bits Manipulation/Set the rightmost unset bit.java
@@ -1,1 +1,9 @@
 // Problem Link: https://www.geeksforgeeks.org/problems/set-the-rightmost-unset-bit4436/1
+
+// User function Template for Java
+class Solution {
+    static int setBit(int n) {
+        // code here
+        return (n|(n+1));
+    }
+}
