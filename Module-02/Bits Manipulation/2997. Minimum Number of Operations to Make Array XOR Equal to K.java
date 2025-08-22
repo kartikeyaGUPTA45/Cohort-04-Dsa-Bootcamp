be0ea407548402a2180784673170f57e4b36cbb8
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-22 22:58:50 +0530

    Update 2997. Minimum Number of Operations to Make Array XOR Equal to K.java

--- Module-02/Bits Manipulation/2997. Minimum Number of Operations to Make Array XOR Equal to K.java
+++ Module-02/Bits Manipulation/2997. Minimum Number of Operations to Make Array XOR Equal to K.java
@@ -1,1 +1,22 @@
 
+class Solution {
+
+    private int csb(int n) {
+        int ans = 0;
+        while(n > 0) {
+            ans += 1;
+            n = (n&(n-1));
+        }
+
+        return ans;
+    }
+
+    public int minOperations(int[] nums, int k) {
+        int xor = 0;
+        for(int ele:nums) {
+            xor ^= ele;
+        }
+        xor^=k;
+        return csb(xor);
+    }
+}
