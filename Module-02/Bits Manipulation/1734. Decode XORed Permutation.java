1019aa6dd1f290c57c7ad2f0eec2cb5ea5607684
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-22 22:38:20 +0530

    Update 1734. Decode XORed Permutation.java

--- Module-02/Bits Manipulation/1734. Decode XORed Permutation.java
+++ Module-02/Bits Manipulation/1734. Decode XORed Permutation.java
@@ -1,1 +1,26 @@
 
+class Solution {
+    public int[] decode(int[] encoded) {
+        int n = encoded.length;
+
+        int first = 0;
+        for(int i=1;i<=(n+1);i++) {
+            first^=i;
+        }
+
+        for(int i=1;i<n;i+=2) {
+            first ^= encoded[i];
+        }
+
+        int perm[] = new int[n+1];
+        perm[0] = first;
+
+        for(int i=0;i<n;i++) {
+            perm[i+1] = encoded[i]^perm[i];
+        }
+
+        return perm;
+    }
+}
+
+
