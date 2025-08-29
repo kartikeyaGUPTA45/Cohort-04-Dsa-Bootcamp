bb572adb7425e3cb39ac286aa7d3c2d250c4646b
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-29 23:23:12 +0530

    Update 1608. Special Array With X Elements Greater Than or Equal X.java

--- Module-02/Searching/1608. Special Array With X Elements Greater Than or Equal X.java
+++ Module-02/Searching/1608. Special Array With X Elements Greater Than or Equal X.java
@@ -1,1 +1,35 @@
 
+class Solution {
+
+    public int countGORE(int nums[], int ele) {
+        int x = 0;
+        for(int i=0;i<nums.length;i++) {
+            if (nums[i] >= ele) {
+                x+=1;
+            }
+        }
+
+        return x;
+    }
+
+    public int specialArray(int[] nums) {
+        int low = 0;
+        int high = 1000;
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            int x = countGORE(nums, mid);
+
+            if (mid == x) {
+                return mid;
+            } else if (mid > x) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return -1;
+    }
+}
