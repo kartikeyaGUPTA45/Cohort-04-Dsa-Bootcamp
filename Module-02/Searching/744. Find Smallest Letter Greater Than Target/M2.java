8b4348ba5322a3daa25477a391424dfd7c4a25eb
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-29 23:45:53 +0530

    Create M2.java

---
+++ Module-02/Searching/744. Find Smallest Letter Greater Than Target/M2.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0;
+        int high = letters.length-1;
+        char ans = '-';
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            if (letters[mid] > target) {
+                ans = letters[mid];
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return (ans == '-' ? letters[0] : ans);
+    }
+}
