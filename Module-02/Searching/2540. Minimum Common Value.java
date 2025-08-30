e98be962a7baa88f7ef9ebb990f4ae950216278e
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-08-30 23:56:43 +0530

    Update 2540. Minimum Common Value.java

--- Module-02/Searching/2540. Minimum Common Value.java
+++ Module-02/Searching/2540. Minimum Common Value.java
@@ -1,1 +1,33 @@
+class Solution {
 
+    public int bs(int arr[], int x) {
+        int low = 0;
+        int high = arr.length-1;
+
+        while(low <= high) {
+            int mid = (low + high)/2;
+
+            if (arr[mid] == x) {
+                return mid;
+            } else if (arr[mid] > x) {
+                high = mid-1;
+            } else {
+                low = mid+1;
+            }
+        }
+
+        return -1;
+    }
+
+    public int getCommon(int[] nums1, int[] nums2) {
+        for(int i=0;i<nums1.length;i++) {
+            int x = nums1[i];
+            int idx = bs(nums2, x);
+            if (idx != -1) {
+                return x;
+            }
+        }
+
+        return -1;
+    }
+}
