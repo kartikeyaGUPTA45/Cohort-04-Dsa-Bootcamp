e5e5cd002adc23ee7302181d393154a7e4be4711
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-09-05 22:31:58 +0530

    Update 878. Nth Magical Number.java

--- Module-02/Searching/878. Nth Magical Number.java
+++ Module-02/Searching/878. Nth Magical Number.java
@@ -1,1 +1,39 @@
 
+class Solution {
+
+    public long gcd(long A, long B) {
+        if (B == 0) {
+            return A;
+        }
+
+        return gcd(B, A%B);
+    }
+
+    public int nthMagicalNumber(int n, int a, int b) {
+        long A = a;
+        long B = b;
+        long N = n;
+
+        long low = Math.min(A,B);
+        long high = N*Math.min(A,B);
+        long lcm = (A*B)/gcd(A,B);
+        long ans = 0;
+
+        while(low <= high) {
+            long mid = low + (high - low)/2;
+
+            long cntMagicalNumberTillMid = (mid/A) + (mid/B) - (mid/lcm);
+
+            if(N == cntMagicalNumberTillMid) {
+                ans = mid;
+                high = mid-1;
+            } else if (N > cntMagicalNumberTillMid) {
+                low = mid+1;
+            } else {
+                high = mid-1;
+            }
+        }
+
+        return (int)(ans % 1000000007);
+    }
+}
