b413e1399d117107b3a563d44f91d073c0c4764e
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-10-03 22:04:51 +0530

    Create Selection sort.java

---
+++ Module-02/Sorting/Selection sort.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    
+    public static void swap(int arr[], int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    
+    public static void printTheArray(int arr[], int n) {
+        for(int i=0;i<n;i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+    
+    public static void selectionSort(int arr[], int n) {
+        for(int i=0;i<n-1;i++) {
+            int minIdx = i;
+            for(int j=i+1;j<n;j++) {
+                if (arr[minIdx] > arr[j]) {
+                    minIdx = j;
+                }
+            }
+            
+            swap(arr,minIdx, i);
+        }
+    }
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int arr[] = new int[n];
+        
+        for(int i=0;i<n;i++) {
+            arr[i] = scn.nextInt();
+        }
+        
+        selectionSort(arr, n);
+        printTheArray(arr, n);
+        
+    }
+}
