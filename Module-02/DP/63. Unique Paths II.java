382b5c6081666b1c863f901d607625d4fa578421
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-11-14 21:32:17 +0530

    Update 63. Unique Paths II.java

--- Module-02/DP/63. Unique Paths II.java
+++ Module-02/DP/63. Unique Paths II.java
@@ -1,5 +1,57 @@
 class Solution {
 
+    private int up_spo(int m, int n, int grid[][]) {
+        int dp[] = new int[n];
+
+        if (grid[m-1][n-1] == 1) {
+            return 0;
+        }
+
+        dp[n-1] = 1;
+
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if (grid[i][j] == 1) {
+                    dp[j] = 0;
+                } else {
+                    int rightPaths = (j+1 < n) ? dp[j+1] : 0;
+                    int downPaths = dp[j];
+
+                    dp[j] = rightPaths + downPaths;
+                }
+            }
+        }
+
+        return dp[0];
+    }
+
+    private int up_tab(int m, int n, int grid[][], int dp[][]) {
+        if (grid[m-1][n-1] == 1) {
+            return 0;
+        }
+
+        dp[m-1][n-1] = 1;
+
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if (i == m-1 && j == n-1) {
+                    continue;
+                }
+
+                if (grid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else {
+                    int rightPaths = (j+1 < n) ? dp[i][j+1] : 0;
+                    int downPaths = (i+1 < m) ? dp[i+1][j] : 0;
+
+                    dp[i][j] = rightPaths + downPaths;
+                }
+            }
+        }
+
+        return dp[0][0];
+    }
+
     private int up_memo(int row, int col, int m, int n, int grid[][], int dp[][]) {
         if (row >= m || col >= n || grid[row][col] == 1) {
             return 0;
@@ -46,6 +98,6 @@ class Solution {
         }
 
 
-        return up_memo(0,0,m,n,grid,dp);
+        return up_spo(m,n,grid);
     }
 }
