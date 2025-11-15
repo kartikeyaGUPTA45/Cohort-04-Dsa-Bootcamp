1c159bcf060bc5e75c82ff0bdf129fd79e3f8073
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-11-15 22:12:33 +0530

    Create 516. Longest Palindromic Subsequence.java

---
+++ Module-02/DP/516. Longest Palindromic Subsequence.java
@@ -0,0 +1,30 @@
+class Solution {
+
+    private int lcs(String t1, String t2) {
+        int m = t1.length();
+        int n = t2.length();
+
+        int curr[] = new int [n+1];
+        int next[] = new int [n+1];
+
+        for(int i=m-1;i>=0;i--) {
+            for(int j=n-1;j>=0;j--) {
+                if (t1.charAt(i) == t2.charAt(j)) {
+                    curr[j] = next[j+1] + 1;
+                } else {
+                    curr[j] = Math.max(curr[j+1], next[j]);
+                }
+            }
+
+            next = curr.clone();
+        }
+
+        return curr[0];
+    }
+
+    public int longestPalindromeSubseq(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        String t = sb.reverse().toString();
+        return lcs(s,t);
+    }
+}
