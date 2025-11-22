0aa99e40f5dfc945a60dcf4124e942e4592ddda7
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-11-22 21:28:20 +0530

    Update 300. Longest Increasing Subsequence.java

--- Module-02/DP/300. Longest Increasing Subsequence.java
+++ Module-02/DP/300. Longest Increasing Subsequence.java
@@ -61,15 +61,36 @@ class Solution {
         return dp[idx][prev+1] = ans;
     }
 
+    private int lis_tab(int nums[]) {
+        int n = nums.length;
+        int dp[][] = new int[n+1][n+1];
+
+        for(int idx = n-1;idx>=0;idx--) {
+            for(int prev = idx-1;prev>=-1;prev--) {
+                int take = 0;
+                if (prev == -1 || nums[idx] > nums[prev]) {
+                    take = dp[idx+1][idx+1] + 1;
+                }
+
+                int notTake = dp[idx+1][prev+1];
+                dp[idx][prev+1] = Math.max(take, notTake);
+            }
+        }
+
+        return dp[0][0];
+
+    }
+
     public int lengthOfLIS(int[] nums) {
         int n = nums.length;
         // int dp[][] = new int[n+1][n+1]; // lis_memo
-        int dp[][] = new int[n][n+1]; // lis_memo2
+        int dp[][] = new int[n+1][n+1]; // lis_memo2
         for(int a[]: dp) {
             Arrays.fill(a, -1);
         }
 
         // return lis_rec(nums, 0, -1);
-        return lis_memo2(nums, 0, -1, dp);
+        // return lis_memo2(nums, 0, -1, dp);
+        return lis_tab(nums);
     }
 }
