cc5c9c3aeebe21d52f7aef3fc272cbb47261b9e6
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-11-29 22:31:19 +0530

    Create 1547. Minimum Cost to Cut a Stick.java

---
+++ Module-02/DP/1547. Minimum Cost to Cut a Stick.java
@@ -0,0 +1,52 @@
+class Solution {
+
+    private int mc_rec(int n, int si, int ei, int m, int cuts[]) {
+        if (si >= ei) {
+            return 0;
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int idx = si;idx<ei;idx++) {
+            int ls = (si == 0) ? 0 : cuts[si-1];
+            int re = (ei == m) ? n : cuts[ei];
+            int cost = re - ls;
+            int subAns = mc_rec(n,si,idx,m,cuts) + mc_rec(n,idx+1,ei,m,cuts) + cost;
+            ans = Math.min(ans, subAns);
+        }
+
+        return ans;
+    }
+
+    private int mc_memo(int n, int si, int ei, int m, int cuts[], int dp[][]) {
+        if (si >= ei) {
+            return 0;
+        }
+
+        if (dp[si][ei] != -1) {
+            return dp[si][ei];
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int idx = si;idx<ei;idx++) {
+            int ls = (si == 0) ? 0 : cuts[si-1];
+            int re = (ei == m) ? n : cuts[ei];
+            int cost = re - ls;
+            int subAns = mc_memo(n,si,idx,m,cuts,dp) + mc_memo(n,idx+1,ei,m,cuts,dp) + cost;
+            ans = Math.min(ans, subAns);
+        }
+
+        return dp[si][ei] = ans;
+    }
+
+    public int minCost(int n, int[] cuts) {
+        int m = cuts.length;
+        Arrays.sort(cuts);
+        int dp[][] = new int[m+1][m+1];
+        for(int a[]: dp) {
+            Arrays.fill(a, -1);
+        }
+
+        // return mc_rec(n,0,m,m,cuts);
+        return mc_memo(n,0,m,m,cuts,dp);
+    }
+}
