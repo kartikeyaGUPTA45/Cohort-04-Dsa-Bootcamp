e824e54ff0b1f9abc92d84783d74c321dd71be08
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-11-29 22:49:56 +0530

    Update 1547. Minimum Cost to Cut a Stick.java

--- Module-02/DP/1547. Minimum Cost to Cut a Stick.java
+++ Module-02/DP/1547. Minimum Cost to Cut a Stick.java
@@ -38,15 +38,35 @@ class Solution {
         return dp[si][ei] = ans;
     }
 
+    private int mc_tab(int n, int m, int cuts[], int dp[][]) {
+        for(int si = m-1;si>=0;si--) {
+            for(int ei = si+1;ei<=m;ei++) {
+                int ans = Integer.MAX_VALUE;
+                for(int idx = si;idx<ei;idx++) {
+                    int ls = (si == 0) ? 0 : cuts[si-1];
+                    int re = (ei == m) ? n : cuts[ei];
+                    int cost = re - ls;
+                    int subAns = dp[si][idx] + dp[idx+1][ei] + cost;
+                    ans = Math.min(ans, subAns);
+                }
+
+                dp[si][ei] = ans;
+            }
+        }
+
+        return dp[0][m];
+    }
+
     public int minCost(int n, int[] cuts) {
         int m = cuts.length;
         Arrays.sort(cuts);
         int dp[][] = new int[m+1][m+1];
-        for(int a[]: dp) {
-            Arrays.fill(a, -1);
-        }
+        // for(int a[]: dp) {
+        //     Arrays.fill(a, -1);
+        // }
 
         // return mc_rec(n,0,m,m,cuts);
-        return mc_memo(n,0,m,m,cuts,dp);
+        // return mc_memo(n,0,m,m,cuts,dp);
+        return mc_tab(n,m,cuts,dp);
     }
 }
