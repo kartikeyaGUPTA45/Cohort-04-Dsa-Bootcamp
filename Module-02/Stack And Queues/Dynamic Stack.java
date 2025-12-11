f5c41d80a4389f6293400117a04f884f108a4c3f
KARTIKEYA GUPTA <55397207+kartikeyaGUPTA45@users.noreply.github.com>
2025-12-11 22:19:12 +0530

    Update Dynamic Stack.java

--- Module-02/Stack And Queues/Dynamic Stack.java
+++ Module-02/Stack And Queues/Dynamic Stack.java
@@ -3,24 +3,52 @@ import java.util.*;
 
 class CustomStack {
   
-  CustomStack(int capacity) {
+  int arr[];
+  int top;
 
+  CustomStack(int capacity) {
+    arr = new int[capacity];
+    top = 0;  
   }
   
   void push(int data) {
-    
+    if (top == arr.length) {
+        int newArr[] = new int[2*arr.length];
+        for(int i=0;i<arr.length;i++) {
+            newArr[i] = arr[i];
+        }
+        
+        arr = newArr;
+    }
+      arr[top] = data;
+      top+=1;
   }
   
   void pop() {
-    
+      if (top == 0) {
+          System.out.println("Stack underflow");
+          return;
+      }
+
+      System.out.println(arr[top-1]);
+      top-=1;
   }
   
   void display() {
-    
+    for(int i = top-1; i>=0;i--) {
+        System.out.print(arr[i] + " ");
+    }
+
+    System.out.println();  
   }
   
   void top() {
-  
+    if (top == 0) {
+          System.out.println("Stack underflow");
+          return;
+     }
+
+      System.out.println(arr[top-1]);
   }
 }
 
